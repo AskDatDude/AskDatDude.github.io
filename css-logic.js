ccf6f612703fae36db055109a2428e954e32d49d
--robin-- <robba355@gmail.com>
2023-06-18 17:55:37 +0300

    .

--- css-logic.js
+++ css-logic.js
@@ -2,11 +2,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementsByClassName("top-nav").style.padding = "30px 10px";
-        document.getElementsByClassName("menu").style.fontSize = "25px";
+        document.getElementsById("top-nav").style.padding = "30px 10px";
+        document.getElementsById("menu").style.fontSize = "25px";
 
     } else {
         document.getElementsByClassName("top-nav").style.padding = "80px 10px";
-        document.getElementsByClassName("menu").style.fontSize = "2vw";
+        document.getElementsById("menu").style.fontSize = "2vw";
     }
 }
